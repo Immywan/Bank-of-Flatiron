b21683a43d1837ef8c432ef8560482dda24b407b
Immaculatewangari <immaculate.wangari@student.moringaschool.com>
2024-05-02 10:36:23 +0300

    DONE

---
+++ src/components/AccountContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Search from "./Search";
+
+function AccountContainer() {
+  return (
+    <div>
+      <Search />
+      {/* <AddTransactionForm /> */}
+      {/* <TransactionsList /> */}
+    </div>
+  );
+}
+
+export default AccountContainer;
